34b715f2a86e03af2646ba035f24d60975bc32c2
Super Ninja Fat <paulmlindberg@gmail.com>
2026-03-07 15:51:47 -0500

    Implement powerup and add it to item pools

--- main.lua
+++ main.lua
@@ -35,14 +35,7 @@ do
 	item_texture_id = define_texture(item_texture_def)
 end
 
--- TODO:
---[[
-    - [x] Copy over basic functionality
-    - [x] Make a custom pastebomb entity (with a base entity as paste bomb?)
-    - [ ] Make a custom pickup entity (with a base entity as paste pickup?)
-    - [ ] Make a custom powerup entity (with a base entity as paste powerup?)
-    - [ ] Make only bombs or paste bombs created by a player with the powerup turn into thermal paste bombs. Account for powerpack bombs as well.
-]]
+local powerup_id, pickup_id
 
 --[[
     Bombs use idle_counter to track when they should explode. They
@@ -107,13 +100,85 @@ local item_id = celib.new_custom_entity(
 	celib.UPDATE_TYPE.POST_STATEMACHINE
 )
 
--- celib.add_custom_shop_chance(pickup_id, celib.CHANCE.COMMON, {
--- 	celib.SHOP_TYPE.WEAPON_SHOP,
--- 	celib.SHOP_TYPE.DICESHOP,
--- 	celib.SHOP_TYPE.TUSKDICESHOP,
--- 	celib.SHOP_TYPE.CAVEMAN,
--- }, true)
--- celib.add_custom_container_chance(pickup_id, celib.CHANCE.LOWER, { ENT_TYPE.ITEM_CRATE, ENT_TYPE.ITEM_PRESENT })
+-- Powerup: applied to players, draws HUD icon, grants vanilla paste
+local function powerup_set(ent)
+	ent:give_powerup(ENT_TYPE.ITEM_POWERUP_PASTE)
+	return {}
+end
+
+local function powerup_update(ent, c_data)
+	-- Remove vanilla paste if player dies and loses the custom powerup
+	if test_flag(ent.flags, ENT_FLAG.DEAD) and not ent:has_powerup(ENT_TYPE.ITEM_POWERUP_ANKH) then
+		ent:remove_powerup(ENT_TYPE.ITEM_POWERUP_PASTE)
+	end
+end
+
+powerup_id = celib.new_custom_powerup(powerup_set, powerup_update, powerup_texture_id, 0, 0)
+
+-- Pickup: item on the ground that grants the powerup when touched
+local function pickup_set(ent)
+	celib.set_entity_info_from_custom_id(ent, pickup_id)
+end
+
+local function pickup_update() end
+
+local function pickup_picked(_, player)
+	celib.do_pickup_effect(player.uid, powerup_texture_id, 0)
+end
+
+pickup_id = celib.new_custom_pickup(pickup_set, pickup_update, pickup_picked, powerup_id, ENT_TYPE.ITEM_PICKUP_PASTE)
+celib.set_powerup_drop(powerup_id, pickup_id)
+celib.add_custom_entity_info(pickup_id, "Thermal Paste", powerup_texture_id, 0, 10000, 1000)
+celib.define_custom_entity_tilecode(pickup_id, "thermal_paste", true)
+
+celib.add_custom_shop_chance(pickup_id, celib.CHANCE.COMMON, {
+	celib.SHOP_TYPE.WEAPON_SHOP,
+	celib.SHOP_TYPE.DICESHOP,
+	celib.SHOP_TYPE.TUSKDICESHOP,
+	celib.SHOP_TYPE.CAVEMAN,
+}, true)
+celib.add_custom_container_chance(pickup_id, celib.CHANCE.LOWER, { ENT_TYPE.ITEM_CRATE, ENT_TYPE.ITEM_PRESENT })
+
+-- Convert bombs/paste bombs thrown by players with the powerup into thermal paste bombs
+local function has_thermal_paste_powerup(player_uid)
+	return celib.custom_types
+		and celib.custom_types[powerup_id]
+		and celib.custom_types[powerup_id].entities[player_uid] ~= nil
+end
+
+-- Apply custom entity to paste bombs thrown by players with the thermal paste powerup
+set_post_entity_spawn(function(ent)
+	local checked = false
+	ent:set_pre_update_state_machine(
+		---@param bomb Bomb
+		function(bomb)
+			if checked then
+				return
+			end
+			checked = true
+			local owner_uid = bomb.last_owner_uid
+			if owner_uid == -1 then
+				return
+			end
+			local owner = get_entity(owner_uid)
+			if not owner or owner.type.search_flags ~= MASK.PLAYER then
+				return
+			end
+			if not has_thermal_paste_powerup(owner_uid) then
+				return
+			end
+			celib.set_custom_entity(bomb.uid, item_id)
+		end
+	)
+end, SPAWN_TYPE.ANY, MASK.ITEM, ENT_TYPE.ITEM_PASTEBOMB)
+
+celib.add_custom_shop_chance(pickup_id, celib.CHANCE.COMMON, {
+	celib.SHOP_TYPE.WEAPON_SHOP,
+	celib.SHOP_TYPE.DICESHOP,
+	celib.SHOP_TYPE.TUSKDICESHOP,
+	celib.SHOP_TYPE.CAVEMAN,
+}, true)
+celib.add_custom_container_chance(pickup_id, celib.CHANCE.LOWER, { ENT_TYPE.ITEM_CRATE, ENT_TYPE.ITEM_PRESENT })
 
 celib.init()
 
@@ -123,4 +188,12 @@ register_option_button("thermal_paste_bomb_item_spawn", "Spawn Thermal Paste Bom
 	end
 	local x, y, l = get_position(players[1].uid)
 	celib.set_custom_entity(spawn(ENT_TYPE.ITEM_PASTEBOMB, x, y, l, 0, 0), item_id)
+end)
+
+register_option_button("thermal_paste_pickup_spawn", "Spawn Thermal Paste Pickup", "", function()
+	if #players == 0 then
+		return
+	end
+	local x, y, l = get_position(players[1].uid)
+	celib.spawn_custom_entity(pickup_id, x, y, l, 0, 0)
 end)
